43357fd129507dbf348a22431f88bfd7956ae058
Christine Yi <christineyi@Christines-MacBook-Air.local>
2016-03-23 21:53:06 -0400

    used angular to spell out more data on index.html

--- public/js/app.js
+++ public/js/app.js
@@ -4,27 +4,64 @@ var app = angular.module("champApp", []);
 
 app.controller('ChampController', ['$http', function($http){
 	this.champs = [];
+	this.champ_id = "";
 	var controller = this;
 
-	$http.get('/champ').then(
-		//success
+	// $http.get('/champ').then(
+	// 	//success
+	// 	function(response){
+	// 		// controller.champs = response.data;
+	// 		// console.log(response.data)
+	// 	},
+	// 	//error
+	// 	function(err){
+	// 		console.log(err)
+	// 	});
+
+	$http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=d976480e-3f05-49c4-b516-3abb34d75227")
+	.then(
 		function(response){
-			// controller.champs = response.data;
-			console.log(response.data)
+
+			for (var key in response.data.data){
+				if (response.data.data.hasOwnProperty(key)){
+					var obj = response.data.data[key];
+					for (var prop in obj) {
+						if (obj.hasOwnProperty(prop)){
+							if (prop == "id"){
+								controller.champ_id = obj[prop]
+								console.log(controller.champ_id)
+								// controller.champ_ids.push(obj[prop]);
+								// console.log(controller.champ_ids);
+
+
+								$http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + controller.champ_id +"?champData=stats&api_key=d976480e-3f05-49c4-b516-3abb34d75227")
+									.then(
+										function(response){
+											controller.champs.push(response.data);
+
+											// console.log(controller.champ_ids)
+
+										},
+										function(err){
+											console.log(err)
+									}); //ends http request
+
+
+							}
+						}
+					}
+				}
+			}
 		},
-		//error
 		function(err){
 			console.log(err)
 		});
 
-	$http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/81?champData=stats&api_key=d976480e-3f05-49c4-b516-3abb34d75227")
-		.then(
-			function(response){
-				controller.champs.push(response.data);
-				console.log(response.data)
-			},
-			function(err){
-				console.log(err)
-		});
+
+
+
+
 }]);
+
+
 
