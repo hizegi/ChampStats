980b7c91178ab7a8cc75378b3f189291a665e38c
Christine Yi <christineyi@Christines-MacBook-Air.local>
2016-03-25 19:05:26 -0400

    displaying team info on page properly

--- public/js/team_controllers.js
+++ public/js/team_controllers.js
@@ -6,17 +6,27 @@ app.controller('TeamController', ['$http', '$scope', '$rootScope', '$location', 
 	var controller = this;
 	var userID = $rootScope.user._id;
 	this.teams = [];
+	this.stats = [];
 	this.message = "Hello";
 
 	//grab all the teams from the user, push it into the locations array []
     $http.get("/user/" + userID).then(function(response){
 
     	// console.log("This is the response: ", response)
-        //for each team in the array, push it to this.locations[]
+        //for each team in the array, push each team to array
         for (var i = 0; i < response.data.team.length; i++) {
-            controller.teams.push(response.data.team[i].name)
-            console.log(response.data.team[i].name)
+            controller.teams.push(response.data.team[i])
+            console.log("This is what I pushed in: ", response.data.team[i])
+            console.log("This is the teams array: ", controller.teams)
         }
+
+        //for each team in the array, push teams stats into array
+        for (var i = 0; i < response.data.team.length; i++) {
+            controller.teams.push(response.data.team[i].stats)
+
+            console.log("This is the stats array: ", controller.stats)
+        }
+
     })
 
 
